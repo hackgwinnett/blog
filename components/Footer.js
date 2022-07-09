83b52ee972d6f8556e862e2587254115b761630b
Nathan Abraham <abnathan123@gmail.com>
2022-07-09 17:34:23 -0400

    Change header and footer fonts

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="footer footer-center p-0 bg-primary text-neutral-content shrink-0 mt-8">
+    <footer className="footer footer-center p-0 bg-primary text-neutral-content shrink-0 mt-8 font-['Montserrat']">
       <div>
         <p className="font-bold text-base md:text-lg">Powered by <a className="btn glass p-2 ml-2" href="https://hackgwinnett.github.io" rel="noreferrer" target="_blank">HackGwinnett</a></p>
       </div>
